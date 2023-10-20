c672ae9a07aaf160313f3195c5db6478470d202f
Paul <berguin.paul@gmail.com>
2023-10-20 14:54:29 -0400

    Auto stash before rebase of "origin/Routing-architecture"

--- src/pages/Student/JoinRoom/JoinRoom.tsx
+++ src/pages/Student/JoinRoom/JoinRoom.tsx
@@ -1,9 +1,40 @@
-// JoinRoom.tsx
-import React from 'react';
+import React, { useState } from 'react';
+import io from 'socket.io-client';
 
 const JoinRoom: React.FC = () => {
+    const [password, setPassword] = useState('');
+
+    const handleJoin = () => {
+
+        const socket = io('http://localhost:4400', {
+            transports: ['websocket'],
+            reconnectionAttempts: 1
+        });
+        socket.on('connect', () => {
+            socket.emit('join-room', password);
+        });
+
+        socket.on('join-success', () => {
+            console.log('Successfully joined the room.');
+        });
+
+        socket.on('join-failure', () => {
+            console.log('Failed to join the room.');
+        });
+        socket.on('connect_error', (error) => {
+            console.log("Connection Error:", error);
+        });
+    };
+
     return (
         <div>
+            hello world
+            <input 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter room password"
+            />
+            <button onClick={handleJoin}>Join</button>
         </div>
     );
 };
--- src/pages/Teacher/CreateRoom/CreateRoom.tsx
+++ src/pages/Teacher/CreateRoom/CreateRoom.tsx
@@ -1,9 +1,38 @@
 // CreateRoom.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const CreateRoom: React.FC = () => {
+    const generatePassword = (length = 6) => {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return result;
+    };
+    const password = generatePassword();
+    
+    useEffect(() => {
+        // Send password to backend on component mount
+        fetch('http://localhost:4400/set-room-password', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ password })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Response from server:", data.message);
+        })
+        .catch(error => console.error('Error sending password:', error));
+    }, [password]);
+
+    
     return (
         <div>
+            <h1>Pawword</h1>
+            <span>{password}</span>
         </div>
     );
 };
