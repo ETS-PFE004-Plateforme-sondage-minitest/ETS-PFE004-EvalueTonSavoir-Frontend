a89f884f59686b496f50eeb8e7006cf29dd38c7b
MihaiFloca <mihfloca@gmail.com>
2023-11-05 16:26:00 -0500

    Room name always uppercase

--- src/pages/Student/JoinRoom/JoinRoom.tsx
+++ src/pages/Student/JoinRoom/JoinRoom.tsx
@@ -110,7 +110,7 @@ const JoinRoom: React.FC = () => {
 
                     <input
                         value={roomName}
-                        onChange={(e) => setRoomName(e.target.value)}
+                        onChange={(e) => setRoomName(e.target.value.toUpperCase())}
                         placeholder="Enter room name"
                     />
                     <button onClick={handleSocket}>Join</button>
