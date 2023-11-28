c60cb8d47ae57e83b9bfb90922b367807a401681
bavi03 <bavithra.jeyarasa.1@ens.etsmtl.ca>
2023-11-28 15:51:07 -0500

    DashBoard

--- src/__tests__/pages/Teacher/Dashboard/Dashboard.test.tsx
+++ src/__tests__/pages/Teacher/Dashboard/Dashboard.test.tsx
@@ -1,44 +1,67 @@
-import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
 import Dashboard from '../../../../pages/Teacher/Dashboard/Dashboard';
 
-describe('Dashboard Component', () => {
-  const sampleQuiz = {
-    id: '1',
-    title: 'Sample Quiz',
-    questions: ['Question 1', 'Question 2', 'Question 3'],
+const localStorageMock = (() => {
+  let store: Record<string, string> = {};
+  return {
+    getItem: (key: string) => store[key] || null,
+    setItem: (key: string, value: string) => (store[key] = value.toString()),
+    clear: () => (store = {}),
   };
+})();
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}));
+
+
+describe('Dashboard Component', () => {
   beforeEach(() => {
-    localStorage.setItem('quizzes', JSON.stringify([sampleQuiz]));
+    localStorage.setItem('quizzes', JSON.stringify([]));
   });
 
-  test('renders dashboard with initial state', () => {
+  test('renders Dashboard with default state', () => {
+    render(
+      <MemoryRouter>
+        <Dashboard />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/Tableau de bord/i)).toBeInTheDocument();
+  });
+
+  test('adds a quiz and checks if it is displayed', () => {
+    const mockQuizzes = [
+      {
+        id: '1',
+        title: 'Sample Quiz',
+        questions: ['Question 1?', 'Question 2?'],
+      },
+    ];
+    localStorage.setItem('quizzes', JSON.stringify(mockQuizzes));
+
     render(
       <MemoryRouter>
         <Dashboard />
       </MemoryRouter>
     );
 
-    expect(screen.getByText('Tableau de bord')).toBeInTheDocument();
+    expect(screen.getByText(/Sample Quiz/i)).toBeInTheDocument();
   });
 
-  test('handles search input', () => {
+  test('opens ImportModal when "Importer" button is clicked', () => {
     render(
       <MemoryRouter>
         <Dashboard />
       </MemoryRouter>
     );
 
-    const searchInput = screen.getByPlaceholderText('Rechercher un quiz');
-    fireEvent.change(searchInput, { target: { value: 'Sample Quiz' } });
+    fireEvent.click(screen.getByText(/Importer/i));
 
-    const quizTitleToSearch = 'Sample Quiz';
-
-    const quizTitleElement = screen.getByText(quizTitleToSearch);
-    expect(quizTitleElement).toBeInTheDocument();
+    expect(screen.getByText(/Importation de quiz/i)).toBeInTheDocument();
   });
-
 
 });
