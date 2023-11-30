89b7dbc907f78ecf3459216c12b0e15eb389924b
MihaiFloca <mihfloca@gmail.com>
2023-11-30 15:22:13 -0500

    Fix users wait page???

--- src/components/UserWaitPage/UserWaitPage.tsx
+++ src/components/UserWaitPage/UserWaitPage.tsx
@@ -2,7 +2,7 @@ import { Button, Chip, Grid } from '@mui/material';
 import { UserType } from '../../Types/UserType';
 import { PlayArrow } from '@mui/icons-material';
 import LaunchQuizDialog from '../LaunchQuizDialog/LaunchQuizDialog';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 
 interface Props {
     users: UserType[];
@@ -13,20 +13,6 @@ interface Props {
 
 const UserWaitPage: React.FC<Props> = ({ users, launchQuiz, roomName, setQuizMode }) => {
     const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
-
-    const gridXS = useMemo(() => {
-        let size = 2;
-        if (users.length <= 2) {
-            size = 8;
-        } else if (users.length <= 3) {
-            size = 6;
-        } else if (users.length <= 6) {
-            size = 4;
-        } else if (users.length < 12) {
-            size = 3;
-        }
-        return size;
-    }, [users]);
 
     return (
         <div className="manage-room-container">
@@ -51,14 +37,13 @@ const UserWaitPage: React.FC<Props> = ({ users, launchQuiz, roomName, setQuizMod
                     spacing={2}
                     sx={{
                         overflowY: 'auto',
-                        height: '60vh',
                         width: '100%',
                         paddingRight: '1rem',
                         paddingTop: '1rem'
                     }}
                 >
                     {users.map((user, index) => (
-                        <Grid item key={user.name + index} xs={gridXS}>
+                        <Grid item key={user.name + index} xs={2}>
                             <Chip label={user.name} sx={{ width: '100%' }} />
                         </Grid>
                     ))}
