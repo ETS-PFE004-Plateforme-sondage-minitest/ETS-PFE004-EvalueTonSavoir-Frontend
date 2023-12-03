2699ca60fe8041d9e9be984040c71747231f83ec
bavi03 <bavithra.jeyarasa.1@ens.etsmtl.ca>
2023-12-02 23:38:49 -0500

    tests import et gift

--- src/__tests__/components/ImportModal/ImportModal.test.tsx
+++ src/__tests__/components/ImportModal/ImportModal.test.tsx
@@ -3,22 +3,25 @@ import '@testing-library/jest-dom';
 import DragAndDrop from '../../../components/ImportModal/ImportModal';
 
 describe('DragAndDrop Component', () => {
-  test('renders without crashing', () => {
-    render(
-      <DragAndDrop
-        handleOnClose={() => {}}
-        handleOnImport={() => {}}
-        open={true}
-      />
-    );
 
-    expect(screen.getByText('Importation de quiz')).toBeInTheDocument();
+  it('renders without errors', () => {
+     const handleOnClose = jest.fn();
+     const handleOnImport = jest.fn();
+     const open = true;
+     render(
+       <DragAndDrop
+        handleOnClose={handleOnClose}
+        handleOnImport={handleOnImport}
+        open={open} />
+      );
+     expect(screen.getByText('Importation de quiz')).toBeInTheDocument();
   });
+
+
 
   test('handles drag and drop', () => {
     const handleOnCloseMock = jest.fn();
     const handleOnImportMock = jest.fn();
-
     render(
       <DragAndDrop
         handleOnClose={handleOnCloseMock}
@@ -26,15 +29,40 @@ describe('DragAndDrop Component', () => {
         open={true}
       />
     );
-
     const dropZone = screen.getByText(/Déposer des fichiers ici/i);
     fireEvent.dragEnter(dropZone);
     fireEvent.dragOver(dropZone);
     fireEvent.drop(dropZone, { dataTransfer: { files: [new File([''], 'sample.txt')] } });
-
     expect(screen.getByText('📄')).toBeInTheDocument();
     expect(screen.getByText('sample.txt')).toBeInTheDocument();
   });
-
-
+  it('handles cancel button correctly', () => {
+    const handleOnClose = jest.fn();
+    const handleOnImport = jest.fn();
+    const open = true;
+    const { container } = render(
+      <DragAndDrop handleOnClose={handleOnClose} handleOnImport={handleOnImport} open={open} />
+    );
+    const file = new File(['file content'], 'example.txt', { type: 'text/plain' });
+    fireEvent.change(screen.getByText( /cliquez pour ouvrir l'explorateur des fichiers/i), {
+      target: { files: [file] },
+    });
+    fireEvent.click(screen.getByText('Annuler'));
+    expect(container.querySelector('.file-container')).not.toBeInTheDocument();
+  });
+  it('handles import correctly', async () => {
+    const handleOnCloseMock = jest.fn();
+    const handleOnImportMock = jest.fn();
+    render(
+      <DragAndDrop
+        handleOnClose={handleOnCloseMock}
+        handleOnImport={handleOnImportMock}
+        open={true}
+      />
+    );
+    const file = new File(['file content'], 'example.txt', { type: 'text/plain' });
+    fireEvent.change(screen.getByText( /Importer/i), {
+      target: { files: [file] },
+    });
+  });
 });
