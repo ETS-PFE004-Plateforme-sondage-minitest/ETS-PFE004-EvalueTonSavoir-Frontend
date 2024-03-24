cfe676e6411f283a2563046153283ddaa418bb48
Louis-Antoine Caron <louis-antoine.caron.1@ens.etsmtl.ca>
2024-03-23 20:10:38 -0400

    fix double rendering

--- src/main.tsx
+++ src/main.tsx
@@ -29,11 +29,9 @@ const theme = createTheme({
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
         <BrowserRouter>
             <ThemeProvider theme={theme}>
                 <App />
             </ThemeProvider>
         </BrowserRouter>
-    </React.StrictMode>
 );
